86df4fb872738877679d8d6faed72f7b4d37d00a
Farhan Abuhazan <farhan.abuhazan@gmail.com>
2018-04-28 16:38:09 +0700

    calibration

--- src/ws/control/control-api.js
+++ src/ws/control/control-api.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var GetControl   = require('../../mcu/mcu').GetControl;
+var GetCalibration   = require('../../mcu/mcu').GetCalibration;
 var GetWaterControl   = require('../../mcu/mcu').GetWaterControl;
+
 var SendCommand  = require('../../mcu/mcu').SendCommand;
 var SendWater =  require('../../mcu/mcu').SendWaterProcess;
+var SendCalibration = require('../../mcu/mcu').SendCalibration;
 
 router.get('/', function(req,res){
     console.log('[Info] Control API: Request');
@@ -14,8 +17,18 @@ router.get('/', function(req,res){
 router.post('/', function(req,res){
     // console.log(req.body);
     SendCommand(req.body.control);
-    res.json({msg: 'successfull'})
+    res.json({msg: 'success'})
 });
+
+router.get('/calibration', (req,res)=>{
+    var cal = GetCalibration();
+    res.json(cal);
+});
+router.post('/calibration', (req,res)=>{
+    SendCalibration(req.body);
+    res.json({msg: 'success'})
+});
+
 
 router.get('/water', (req,res)=>{
     var waterCtrl = GetWaterControl();
@@ -25,7 +38,7 @@ router.get('/water', (req,res)=>{
 router.post('/water', (req,res)=>{
 //    console.log(req.body);
    SendWater(req.body);
-   res.json({msg: 'successfull'})
+   res.json({msg: 'success'})
 });
 
 module.exports = router;
