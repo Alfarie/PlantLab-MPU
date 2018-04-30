9babc82ccd2b4a7e331c3725b380cfbfbf8eec41
Farhan Abuhazan <farhan.abuhazan@gmail.com>
2018-04-30 20:49:47 +0700

    add activity

--- src/ws/setting/setting-api.js
+++ src/ws/setting/setting-api.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var SendDateTime = require('../../mcu/mcu').SendDateTime;
+var activity = require('../../mpu-store/activity.js');
 
 router.post('/datetime', function(req,res){
     SendDateTime(req.body);
     res.json({msg: 'success'})
 });
+
+router.get('/plants', function(req,res){
+    res.json(activity.plant);
+})
+
+router.post('/activity', function(req,res){
+    activity.SaveActivity(req.body);
+    res.json({msg: 'success'})
+})
+router.get('/activity', function(req,res){
+    
+    res.json(activity.GetActivity());
+})
+
+
 
 module.exports = router;
