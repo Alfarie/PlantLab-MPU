50ecfa6b421e5749c8673809187dc33323f87c22
Farhan Abuhazan <farhan.abuhazan@gmail.com>
2018-05-06 14:47:22 +0700

    add wifi

--- src/wifi/wifi.js
+++ src/wifi/wifi.js
@@ -1,11 +1,23 @@
 var wpa_cli = require('wireless-tools/wpa_cli');
-var wpa_supplicant = require('wireless-tools/wpa_supplicant');
+
+var state = 'DISCONNECTED';
+var fs = require('fs');
+// var wpa_supplicant_dir = __dirname + '/wpa.conf';
+var wpa_supplicant_dir = "/etc/wpa_supplicant/wpa_supplicant.conf"
+var template = `ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=GB
+`
+var interface = 'wlan0'
+const {
+    execSync
+} = require('child_process');
 
 
 function ScanWifi() {
     return new Promise((resolve, reject) => {
-        wpa_cli.scan('wlp3s0', function (err, data) {
-            wpa_cli.scan_results('wlp3s0', function (err, data) {
+        wpa_cli.scan(interface, function (err, data) {
+            wpa_cli.scan_results(interface, function (err, data) {
                 if (err) reject(err.message);
                 resolve(data);
             })
@@ -13,6 +25,56 @@ function ScanWifi() {
     });
 }
 
+function CurrentWifi(){
+    return new Promise( (resolve, reject)=>{
+        wpa_cli.status(interface, function(err, status) {
+            if (err) reject(err.message);
+            resolve(status);
+        });
+    })
+}
+
+function Connect(wifi) {
+    /*
+        {ssid: '...', psk: '...'}
+    */
+    //create wpa.conf
+
+    fs.writeFileSync(wpa_supplicant_dir, template);
+    var cmd = 'wpa_passphrase "' + wifi.ssid + '" "' + wifi.psk + '"';
+    var stdout = execSync(cmd).toString();
+    fs.appendFileSync(wpa_supplicant_dir, stdout);
+    stdout = execSync('wpa_cli -i wlan0 reconfigure');
+    return CheckStatus();
+}
+
+function Disconnet() {
+    fs.writeFileSync(wpa_supplicant_dir, template);
+    stdout = execSync('wpa_cli -i wlan0 reconfigure');
+}
+
+function CheckStatus(){
+    return new Promise( (resolve, reject)=>{
+        setTimeout( ()=>{
+            wpa_cli.status(interface, function (err, status) {
+                if(err) console.log(err);
+                console.log(status);
+                if(status.wpa_state == 'COMPLETED'){
+                    resolve(status);
+                }
+                else{
+                    reject(status);
+                }
+            });
+        },5000);
+    });
+}
+
+
+
 module.exports = {
-    ScanWifi
+    ScanWifi,
+    CurrentWifi,
+    Connect,
+    Disconnet
 }
--- src/ws/setting/setting-api.js
+++ src/ws/setting/setting-api.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var SendDateTime = require('../../mcu/mcu').SendDateTime;
 var activity = require('../../mpu-store/activity.js');
 var wifi = require('../../wifi/wifi');
-
 router.post('/datetime', function (req, res) {
     SendDateTime(req.body);
     res.json({
@@ -20,22 +19,30 @@ router.post('/activity', function (req, res) {
     res.json({
         msg: 'success'
     })
-})
+});
+
 router.get('/activity', function (req, res) {
     res.json(activity.GetActivity());
-})
+});
 
 router.get('/wifi/scan', function (req, res) {
     wifi.ScanWifi()
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err=>{
-            res.json({msg: err });
-        })
+    .then(data => {
+        res.json({msg: 'success', data: data});
+    })
+    .catch(err=>{
+        res.json({msg: 'failure', data: err });
+    })
 });
 
-
-
+router.get('/wifi/current',function (req, res) {
+    wifi.CurrentWifi()
+        .then(data => {
+            res.json({msg: 'success', data: data});
+        })
+        .catch(err=>{
+            res.json({msg: 'failure', data: err });
+        })
+})
 
 module.exports = router;
