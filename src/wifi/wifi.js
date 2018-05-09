c359c8a43e775b7a6db4b5bafec9458d0f0c6566
Farhan Abuhazan <farhan.abuhazan@gmail.com>
2018-05-09 11:15:11 +0700

    wifi management update

--- src/wifi/wifi.js
+++ src/wifi/wifi.js
@@ -18,7 +18,10 @@ function ScanWifi() {
     return new Promise((resolve, reject) => {
         wpa_cli.scan(interface, function (err, data) {
             wpa_cli.scan_results(interface, function (err, data) {
-                if (err) reject(err.message);
+                if (err){
+                    reject(err.message);
+                    return;
+                } 
                 resolve(data);
             })
         });
@@ -28,7 +31,10 @@ function ScanWifi() {
 function CurrentWifi(){
     return new Promise( (resolve, reject)=>{
         wpa_cli.status(interface, function(err, status) {
-            if (err) reject(err.message);
+            if (err) {
+                reject(err.message);
+                return;
+            }
             console.log(status);
             if(status.wpa_state == 'COMPLETED'){
                 resolve(status);
@@ -45,7 +51,6 @@ function Connect(wifi) {
         {ssid: '...', psk: '...'}
     */
     //create wpa.conf
-    
     try{
         Disconnet();
         fs.writeFileSync(wpa_supplicant_dir, template);
@@ -58,8 +63,6 @@ function Connect(wifi) {
     catch(e){
         return CheckStatus();
     }
-    
-    
 }
 
 function Disconnet() {
@@ -72,7 +75,11 @@ function CheckStatus(){
     return new Promise( (resolve, reject)=>{
         setTimeout( ()=>{
             wpa_cli.status(interface, function (err, status) {
-                if(err) console.log(err);
+                if(err){
+                    console.log(err);
+                    reject(err);
+                    return;
+                }
                 console.log(status);
                 if(status.wpa_state == 'COMPLETED'){
                     resolve(status);
