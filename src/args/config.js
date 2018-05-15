b6e9c2f077dd2332e7e57455e4a76ded9f52bbe9
Farhan Abuhazan <farhan.abuhazan@gmail.com>
2018-05-15 09:21:23 +0700

    Change iface

--- src/args/config.js
+++ src/args/config.js
@@ -4,7 +4,7 @@ var production = false;
 
 var loggerDirectory = __dirname.replace('/src/args', '/Logger/');
 var loggerTime = 1000 * 60;
-var interface  = 'eth0'
+var interface  = 'enp0s25'
 
 module.exports = {
     port,
